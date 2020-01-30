1d1cdccf4de8b0cb3659686ffb0057a3476bc589
PEC17 <PEC17@skbl-422-014.ph.ic.ac.uk>
2020-01-30 13:04:19 +0000

    LCD module

--- LCD.asm
+++ LCD.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    global  LCD_Setup, LCD_Write_Message, LCD_Clear, LCD_Write_Line1, LCD_Write_Line2
+    global  LCD_Setup, LCD_Write_Message, LCD_Clear, LCD_Write_Line1, LCD_Write_Line2, LCD_Send_Byte_D
 
 acs0    udata_acs   ; named variables in access ram
 LCD_cnt_l   res 1   ; reserve 1 byte for variable LCD_cnt_l
