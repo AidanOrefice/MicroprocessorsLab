4caa0a98be31fe47108344713c1a17d4a75328c8
imperialtest <jason.bennett@imperial.ac.uk>
2020-02-13 17:00:38 +0000

    Start of main code sequence

--- Main.asm
+++ Main.asm
@@ -1,8 +1,24 @@
 #include p18f87k22.inc
     
-extern ADC_Setup, ADC_Read
+
+ 
+
+;extern ADC_Setup, ADC_Read
+    extern  Keyboard_Setup, Keyboard_Read, Store_Decode
+    extern  LCD_Setup, LCD_Write_Message, LCD_Write_Hex
+    extern  LCD_Clear, LCD_Write_Line1, LCD_Write_Line2, LCD_Send_Byte_D, LCD_Preset
+    extern  LCD_delay_ms
     
+    org 0x00
+    goto start
     
+main code
+ 
+ 
+start  call LCD_Setup
+	call LCD_Preset
+    
+    end 
 
 
 
