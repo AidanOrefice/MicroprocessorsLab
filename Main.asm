53b25a34eecd3ba5a9f99caf46ed931555780537
imperialtest <jason.bennett@imperial.ac.uk>
2020-02-18 14:37:00 +0000

    developments for delay

--- Main.asm
+++ Main.asm
@@ -7,7 +7,7 @@
     extern  Keyboard_Setup, Keyboard_Read, Store_Decode, Keyboard_Initial, Delay_Time
     extern  LCD_Setup, LCD_Write_Message, LCD_Write_Hex
     extern  LCD_Clear, LCD_Write_Line1, LCD_Write_Line2, LCD_Send_Byte_D, LCD_Preset, LCD_Delay_Write 
-    extern  LCD_delay_ms
+    extern  LCD_delay_ms, Converted_Delay_Time
     
     
     org 0x00
@@ -16,16 +16,19 @@
 main code
  
  
-start	movlw b'10000000'	    ;This initialises PORTJ such that RJ7 is our switch
-	movwf TRISJ
+start	movlw	b'10000000'	    ;This initialises PORTJ such that RJ7 is our switch
+	movwf	TRISJ
 	
-	call LCD_Setup
-	call LCD_Preset
-	call Keyboard_Setup
-	call Keyboard_Initial
-	btfsc PORTJ, 7
-	call Keyboard_Read
-	call LCD_Delay_Write	    ; This allows us to output the delay time when the switch is down  
-	goto start 
+	clrf	TRISH
+	
+	call	LCD_Setup
+	call	LCD_Preset
+	call	Keyboard_Setup
+	call	Keyboard_Initial
+	btfsc	PORTJ, 7
+	call	Keyboard_Read
+	movff	Converted_Delay_Time, PORTH
+	call	LCD_Delay_Write	    ; This allows us to output the delay time when the switch is down  
+	goto	start 
 
     end
